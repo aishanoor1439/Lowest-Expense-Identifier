a7bb1fe205bfe13e4b3cbcead77301c1439f9ee0
aishanoor1439 <aisha.noor1439@gmail.com>
2024-11-20 09:43:56 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lowest_Expense_Identifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Monthly Expense for Transportation:");
+            double tExp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Monthly Expense for Stationery:");
+            double sExp = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter monthly Expense for Entertainment:");
+            double eExp = Convert.ToDouble(Console.ReadLine());
+            if (tExp < sExp)
+            {
+                if (tExp < eExp)
+                {
+                    Console.WriteLine("Your Transportation Expense is the lowest of all expenses.");
+                }
+                else
+                {
+                    Console.WriteLine("Your Entertainment Expense is the lowest of all expenses.");
+                }
+            }
+            else
+            {
+                if (sExp < eExp)
+                {
+                    Console.WriteLine("Your Stationery Expense is the lowest of all expenses.");
+                }
+                else
+                {
+                    Console.WriteLine("Your Entertainment Expense is the lowest of all expenses.");
+                }
+            }
+        }
+    }
+}
